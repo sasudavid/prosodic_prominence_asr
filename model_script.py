d4dab4625bad6d8ba30582d13028c18d500fb9cb
sasudavid <34894599+sasudavid@users.noreply.github.com>
2025-06-11 13:56:15 +0200

    Update model_script.py

--- model_script.py
+++ model_script.py
@@ -19,6 +19,9 @@ class Wav2Vec2CombinedASR(Wav2Vec2ForCTC):
         self.config.dropout = 0.3
     
     def freeze_base_model_except_head(self):
+        '''
+        Freezes the base wav2vec2 model except the Prosody and ASR Head.
+        '''
         for param in self.parameters():
           param.requires_grad = False
         for param in self.lm_head.parameters():
@@ -26,19 +29,35 @@ class Wav2Vec2CombinedASR(Wav2Vec2ForCTC):
         return
     
     def unfreeze_base_model(self):
+        '''
+        Unfreezes the wav2vec2 model but keeps the feature encoder frozen.
+        '''
         for param in self.parameters():
           param.requires_grad = True
         super().freeze_feature_encoder()
         return
 
     def normalize(self, tensor):
+        '''
+        Normalizes a given input tensor
+
+        Params:
+            tensor (obj): The input tensor object.
+
+        Returns:
+            Normalized tensor object.
+        '''
         mean = tensor.mean()
         std = tensor.std()
         return (tensor - mean)/std
     
     def randomly_initialize_base_model_head(self):
+        '''
+        Randomly initializes the weights of the ASR and Prosody Labelling heads.
+        '''
         lm_head = self.lm_head
         for module in lm_head.modules():
+            #for linear models, ensure that the weights are drawn from a normal distribution and the bias terms have a zero value.
             if isinstance(module, nn.Linear):
                 module.weight.data.normal_(mean=0.0, std=0.02)
                 if module.bias is not None:
@@ -46,45 +65,80 @@ class Wav2Vec2CombinedASR(Wav2Vec2ForCTC):
         return
 
     def initialize_lstm_model_params(self, lstm_model_parameters_path):
+        '''
+        Initialize the bilstm model with the optimal hyper-parameter values found.
+
+        Params:
+            lstm_model_parameters_path (str): This is the path to where the hyper-parameter values are stored.
+        '''
         lstm_params = torch.load(lstm_model_parameters_path)
         self.lstm.load_state_dict(lstm_params)
         return 
 
 
     def forward(self, input_values, pitch=None, attention_mask=None, asr_labels=None, prosodic_labels=None):
+        #check to see if prosody/ASR labels are provided to help determine whether the prosody/ASR head should be included in output generation.
         if -1 in prosodic_labels:
             include_prosody = False
         else:    
             include_prosody = True
         include_asr = True
+
+        #feed the input values extracted from the audio into the wav2vec2 model
         asr_model_outputs = super().forward(input_values=input_values, attention_mask=attention_mask, labels=asr_labels, output_hidden_states=True, return_dict=True)
+
+        #obtain the last hidden state of the wav2vec2 model
+        #hidden_states = asr_model_outputs.hidden_states[0] (mistake made)
         hidden_states = asr_model_outputs.hidden_states[-1]
-        prosody_logits = None
+
+        
+
+
+        #check to see if you would like to include pitch information.
         if pitch is not None:
+            #If pitch information is to be included, check to see if the pitch length provided is smaller than the time-step length of the hidden states
             if (pitch.shape[1] < hidden_states.shape[1]):
+                #if the pitch is found to be smaller, pad the pitch with zeros to ensure that its length is equal to the time-step length of the hidden states
                 pitch = F.pad(pitch, pad=(0, hidden_states.shape[1] - pitch.shape[1]), mode='constant', value=0)
+            #However, if the pitch length is found to be greater, then you would have to truncate it's length
             elif (pitch.shape[1] > hidden_states.shape[1]):
                 pitch = pitch[:hidden_states.shape[1]]
+
+            #reshape the dimensions of the pitch tensor
             pitch = pitch.reshape(pitch.shape[0], pitch.shape[1],1)
+
+        #append the pitch information as additional information to the last dimension of the hidden state
         tup = (pitch.to(device, dtype=torch.float),hidden_states)
         x_ = torch.cat(tup,2)
+
+        #push the hidden state information together with the pitch into the bilstm to generate logits for the Prosody labels
         #NB: if you choose to remove pitch information you must edit the input dimensions of the LSTM model as well.
         x_lstm, (_, _) = self.lstm(x_)
         prosody_vectors = self.extra_prosody_layer(x_lstm)
         normalized_prosody_vectors = self.normalize(prosody_vectors)
         prosody_logits = self.prosody_classifier(normalized_prosody_vectors)
-        loss = None
+
+        
+        
         if asr_labels is not None or prosodic_labels is not None:
+
+            #initialize the loss
             loss = 0
+            
             if asr_labels is not None and include_asr:
+                #compute the loss for the ASR Head
                 loss += asr_model_outputs.loss
+                
             if prosodic_labels is not None and prosody_logits is not None and include_prosody:
+                #compute the loss for the Prosody Head
                 mse_loss = MSELoss()
                 prosody_logits = prosody_logits.squeeze(2)
                 prosody_target_size = prosodic_labels.shape[1]
                 padding_right = prosody_target_size - prosody_logits.shape[1]
                 prosody_logits = F.pad(prosody_logits, (0, padding_right))
                 loss += mse_loss(prosody_logits, prosodic_labels)
+
+        #return the ASR logits, Prosody Logits and the total loss
         return {
             "asr_output": asr_model_outputs.logits,
             "prosody_logits": prosody_logits,
